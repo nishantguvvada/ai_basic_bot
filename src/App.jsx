0fb3a53dbef9aef41ca9c757c6d911a0b11beb12
unknown <nishant.guvvada@gmail.com>
2025-01-16 21:20:46 +0530

    add alert for error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import axios from 'axios';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
-import.meta.env.VITE_API_KEY;
 
 const systemMessage = {
   "role": "system", "content": "Explain things like you're talking to a software professional with only 1 years of experience. Keep the answer within 50 words limit."
@@ -33,35 +32,41 @@ function App() {
   };
 
   const sendMessageToGpt = async (chatMessages) => {
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if (messageObject.sender === "Virtual Assistant") {
-        role = "assistant";
-      } else {
-        role = "user";
-      }
-      return { role: role, content: messageObject.message}
-    });
+    try {
+      let apiMessages = chatMessages.map((messageObject) => {
+        let role = "";
+        if (messageObject.sender === "Virtual Assistant") {
+          role = "assistant";
+        } else {
+          role = "user";
+        }
+        return { role: role, content: messageObject.message}
+      });
+  
+      const apiBody = JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [systemMessage, ...apiMessages]
+      });
+  
+      const session = await axios.post("https://api.aimlapi.com/chat/completions", apiBody, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${import.meta.env.VITE_API_KEY}`
+        }
+      });
+  
+      setIsTyping(false);
+      // console.log("session",session.data.choices[0].message);
+      setMessages([...chatMessages, {
+        message: session.data.choices[0].message.content,
+        sender: "Virtual Assistant",
+        direction: 0
+      }])
+    } catch(err) {
+      alert("Sorry, GPT API limit exhausted");
+      console.log(err);
+    }
 
-    const apiBody = JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [systemMessage, ...apiMessages]
-    });
-
-    const session = await axios.post("https://api.aimlapi.com/chat/completions", apiBody, {
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${import.meta.env.VITE_API_KEY}`
-      }
-    });
-
-    setIsTyping(false);
-    // console.log("session",session.data.choices[0].message);
-    setMessages([...chatMessages, {
-      message: session.data.choices[0].message.content,
-      sender: "Virtual Assistant",
-      direction: 0
-    }])
 
   };
 
